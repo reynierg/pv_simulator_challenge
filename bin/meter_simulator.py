96520220621596b6a3d2df5a4671c6501ae45dcb
Reynier Garcia <reynierg@protonmail.com>
2020-12-02 05:02:58 +0100

    Scripts to execute meter and pv_simulator

---
+++ bin/meter_simulator.py
@@ -0,0 +1,18 @@
+import inspect
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+top_dir = os.path.dirname(current_dir)
+
+print(f"top_dir: {top_dir}")
+if top_dir not in sys.path:
+    sys.path.insert(0, top_dir)
+
+from services.meter import main
+
+if __name__ == "__main__":
+    try:
+        main.main(sys.argv)
+    except Exception as ex:
+        print(ex)
